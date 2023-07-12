3993e55b4c453b8e7e28f711f27bc4657868529b
bhanudhara <89626052+bhanudhara@users.noreply.github.com>
2023-07-12 15:12:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ var json = $.getJSON(
         );
       });
     }
-  );
+
+    );
   $(document).ready(function () {
+    
+
     $('.combobox').change(function () {
       var selectedFont = $(this).val();
       $('body').css('font-family', selectedFont);
@@ -27,6 +30,7 @@ var json = $.getJSON(
   
       $('head').append(styleTag);
     });
+   
   });
 
   $(document).on("click","#buttontoggle",function(){
@@ -37,69 +41,80 @@ var json = $.getJSON(
   function h1() {
     var el =  document.getElementsByClassName("custom_h1");
     var x = document.getElementById('size1').value;
+    var y = document.getElementById('weight1').value;
+    document.getElementById('h1tag').innerHTML=`This is an h1 tag fs-${x}px & fw-${y}`;
     if(el){
       for(var i = 0; i < el.length; i++){
          el[i].style.fontSize = x + 'px';
+         el[i].style.fontWeight = y;
       }
     } 
   }
   function h2() {
     var el =  document.getElementsByClassName("custom_h2");
     var x = document.getElementById('size2').value;
+    var y = document.getElementById('weight2').value;
+    document.getElementById('h2tag').innerHTML=`This is an h2 tag fs-${x}px & fw-${y}`;
     if(el){
       for(var i = 0; i < el.length; i++){
          el[i].style.fontSize = x + 'px';
+         el[i].style.fontWeight = y;
       }
     } 
   }
   function h3() {
     var el =  document.getElementsByClassName("custom_h3");
     var x = document.getElementById('size3').value;
+    var y = document.getElementById('weight3').value;
+    document.getElementById('h3tag').innerHTML=`This is an h3 tag fs-${x}px & fw-${y}`;
     if(el){
       for(var i = 0; i < el.length; i++){
          el[i].style.fontSize = x + 'px';
+         el[i].style.fontWeight = y;
       }
     } 
   }
   function h4() {
     var el =  document.getElementsByClassName("custom_h4");
     var x = document.getElementById('size4').value;
+    var y = document.getElementById('weight4').value;
+    document.getElementById('h4tag').innerHTML=`This is an h4 tag fs-${x}px & fw-${y}`;
     if(el){
       for(var i = 0; i < el.length; i++){
          el[i].style.fontSize = x + 'px';
+         el[i].style.fontWeight = y;
       }
     } 
   }
   function h5() {
     var el =  document.getElementsByClassName("custom_h5");
     var x = document.getElementById('size5').value;
+    var y = document.getElementById('weight5').value;
+    document.getElementById('h5tag').innerHTML=`This is an h5 tag fs-${x}px & fw-${y}`;
     if(el){
       for(var i = 0; i < el.length; i++){
          el[i].style.fontSize = x + 'px';
+         el[i].style.fontWeight = y;
       }
     } 
   }
   function h6() {
     var el =  document.getElementsByClassName("custom_h6");
     var x = document.getElementById('size6').value;
+    var y = document.getElementById('weight6').value;
+    document.getElementById('h6tag').innerHTML=`This is an h6 tag  fs-${x}px & fw-${y}`;
     if(el){
       for(var i = 0; i < el.length; i++){
          el[i].style.fontSize = x + 'px';
+         el[i].style.fontWeight = y;
       }
     } 
   }
-
-  function getData(form) {
-    var formData = new FormData(form);
-  
-    for (var pair of formData.entries()) {
-      console.log(pair[0] + ": " + pair[1]);
-    }
-  
-    console.log(Object.fromEntries(formData));
+  function changecolor(event)
+  {
+    document.getElementById('color_value').innerText=event.target.value;
+    document.getElementById('btncolor').style.background=event.target.value+"";
+    document.getElementById('btncolor2').style.border=`1px solid ${event.target.value}`;
+    document.getElementById('btncolor2').style.color=`${event.target.value}`;
   }
-  
-  document.getElementById("myForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    getData(e.target);
-  });
+ 
